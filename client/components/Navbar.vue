a7134b9e78d313789d78212e1c2a2875e3768788
kiruthiharan <kiruthiharan@gmail.com>
2020-04-10 19:59:00 +0530

    navbar added

---
+++ client/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <header class="nav-opt-sprite nav-locate-us nav-lang-en nav-ssl nav-unrec">
+            <div class="conatainer-fluid dekstop-nav">
+                <div class="row">
+                    <!-- logo -->
+                    <div class="col-sm-2">
+                        <div class="logo-area">
+                            <a href="#">
+                            <img src="/img/logo.png" alt="Logo" class="img-fluid"/>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- search bar -->
+                    <div class="col-sm-6 pt-0">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 pl-2">
+                        <div class="nav-global-location">
+                            <nuxt-link to="" class="nav-a nav-a-2"></nuxt-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    };
+</script>
