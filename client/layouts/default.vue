606fdce37d227b94ec61f925025e251b1cdeba99
kiruthiharan <kiruthiharan@gmail.com>
2020-04-11 18:11:47 +0530

    navbar and footer done

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -2,15 +2,17 @@
   <div>
     <Navbar />
     <nuxt />
+    <Footer/>
   </div>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar.vue'
-
+import Navbar from '~/components/Navbar.vue';
+import Footer from '~/components/Footer.vue';
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
